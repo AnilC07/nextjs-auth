5b92780cabf9444fb02773bc2a0671a8ad4dc108
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-04-07 00:55:23 +0200

    edit NEXTAUTH_URL

--- next.config.js
+++ next.config.js
@@ -23,7 +23,7 @@ module.exports = (phase) => {
       mongodb_clusterName: "cluster0",
       mongodb_database: "auth-prod",
       NEXTAUTH_SECRET: "thequickbrownfox",
-      NEXTAUTH_URL: "https://nextjs-auth-cgi.vercel.app/",
+      NEXTAUTH_URL: "https://nextjs-auth-cgi.vercel.app/auth",
     },
   };
 };
