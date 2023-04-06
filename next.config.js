2a5b943ac68bd0acced333d0d8e521a062781790
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-04-06 23:40:23 +0200

    add NEXTAUTH env variable

--- next.config.js
+++ next.config.js
@@ -8,19 +8,19 @@ module.exports = (phase) => {
         mongodb_password: "x4NfcEB6mz5BZGeE",
         mongodb_clusterName: "cluster0",
         mongodb_database: "auth-prod",
-        NEXTAUTH_SECRET:'thequickbrownfox',
-        NEXTAUTH_URL:'https://nextjs-auth-cgi.vercel.app/auth'
+        NEXTAUTH_SECRET: "thequickbrownfox",
+        NEXTAUTH_URL: "https://nextjs-auth-cgi.vercel.app/auth",
       },
     };
-  }else{
+  } else {
     return {
       env: {
         mongodb_username: "anilcelik075",
         mongodb_password: "x4NfcEB6mz5BZGeE",
         mongodb_clusterName: "cluster0",
         mongodb_database: "auth",
-        NEXTAUTH_SECRET:'thequickbrownfox',
-        NEXTAUTH_URL:'https://nextjs-auth-cgi.vercel.app/auth'
+        NEXTAUTH_SECRET: "thequickbrownfox",
+        NEXTAUTH_URL: "https://nextjs-auth-cgi.vercel.app/auth",
       },
     };
   }
